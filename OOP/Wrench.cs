f2854580e8e804cd90cfa1e9defb821febe26346
ariel ben pazi <rbenpazi@gmail.com>
2025-05-19 17:09:15 +0300

    adedd_wrench_class

--- OOP/Wrench.cs
+++ OOP/Wrench.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+public class Wrench : Tool
+{
+    public Wrench(string name, int kg) : base(name, kg)
+    {
+    }
+
+    public override void describe()
+    {
+        Console.WriteLine($"is: {name} is: {kg} kg "); ;
+    }
+
+    public override void use()
+    {
+        Console.WriteLine("is wrench") ;
+    }
+}
