1352effb1129aaa0ae154f8865049c2ca7c806c1
ariel ben pazi <rbenpazi@gmail.com>
2025-05-19 17:16:30 +0300

    adedd_saw_class

---
+++ OOP/Saw.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Saw : Tool
+{
+    public Saw(string name, int kg) : base(name, kg)
+    {
+    }
+
+    public override void describe()
+    {
+        Console.WriteLine($"is: {name} is: {kg} kg "); ;
+    }
+
+    public override void use()
+    {
+        Console.WriteLine($"is {name}");
+    }
+}
