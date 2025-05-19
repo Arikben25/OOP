5e84e1ac1a22ef92da6bd88875cd883d8863b361
ariel ben pazi <rbenpazi@gmail.com>
2025-05-19 16:34:31 +0300

    adedd_base_class

---
+++ OOP/TOOL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public abstract class Tool
+{
+    protected string name;
+    protected int kg;
+
+    public Tool(string name, int kg)
+    {
+        this.name = name;
+        this.kg = kg;
+    }
+
+    public abstract void describe();
+    public abstract void use();
+
+}
