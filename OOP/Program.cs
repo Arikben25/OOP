8245b8a89805cf1d63a0aeaaefc2c2d47d175122
ariel ben pazi <rbenpazi@gmail.com>
2025-05-19 22:46:53 +0300

    adedd_list_of_tolls

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,6 +10,16 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            Hammer hammer = new Hammer("hammer", 5);
+            Wrench wrench = new Wrench("wrench", 1);
+            Drill drill = new Drill("drill", 2);
+            Saw saw = new Saw("saw", 3);
+            Screwdriver screwdriver = new Screwdriver("scr", 4);
+            PipeCutter pipe = new PipeCutter("pipe", 8);
+            List <Tool> my_tools = new List<Tool> { hammer, wrench, drill, saw, screwdriver, pipe };
+
+            
+   
 
         }
     }
