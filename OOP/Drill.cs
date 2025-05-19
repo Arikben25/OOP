743d8e884a28e95f6a719c77d29667dd70074011
ariel ben pazi <rbenpazi@gmail.com>
2025-05-19 23:07:59 +0300

    create_a_loop

--- OOP/Drill.cs
+++ OOP/Drill.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+public class Drill : Tool
+{
+    public Drill(string name, int kg) : base(name, kg)
+    {
+    }
+
+    public override void describe()
+    {
+        Console.WriteLine($"is: {name} is: {kg} kg "); ;
+    }
+
+    public override void use()
+    {
+        Console.WriteLine($"is {name}");
+    }
+}
--- OOP/PipeCutter.cs
+++ OOP/PipeCutter.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+
+public class PipeCutter : Tool
+{
+    public PipeCutter(string name, int kg) : base(name, kg)
+    {
+    }
+
+    public override void describe()
+    {
+        Console.WriteLine($"is: {name} is: {kg} kg ");
+    }
+
+    public override void use()
+    {
+        Console.WriteLine($"is {name}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
